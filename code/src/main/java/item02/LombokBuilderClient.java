767081136b6bb6543662159505a486c6dc64f6a8
wonsangl <wonsangl@netmarble.com>
2022-08-02 18:53:13 +0900

    Item 02 Lombok 예제 코드 추가

---
+++ code/src/main/java/item02/LombokBuilderClient.java
@@ -0,0 +1,8 @@
+package item02;
+
+public class LombokBuilderClient {
+    public static void main(String[] args) {
+        LombokBuilderTest test = LombokBuilderTest.builder()
+                .build();
+    }
+}
---
+++ code/src/main/java/item02/LombokBuilderTest.java
@@ -0,0 +1,9 @@
+package item02;
+
+import lombok.Builder;
+
+@Builder
+public class LombokBuilderTest {
+    private final String code;
+    private String name;
+}
