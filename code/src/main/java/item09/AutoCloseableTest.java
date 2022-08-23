f5ee3d9962904c2bde5d4c7651e77831b0589d74
wonsangl <wonsangl@netmarble.com>
2022-08-23 20:45:42 +0900

    suppressed exception 예제 코드 수정

--- code/src/main/java/item09/AutoCloseableTest.java
+++ code/src/main/java/item09/AutoCloseableTest.java
@@ -1,6 +1,7 @@
 package item09;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
@@ -20,8 +21,19 @@ public class AutoCloseableTest {
     }
 
     private static void autoCloseTest() throws IOException {
-        try (BufferedReader reader = new BufferedReader(new FileReader("./invalid.txt"))) {
-            reader.readLine();
+        try (BufferedReader reader = new BufferedReader(new TestFileReader("./item_019.md"))) {
+            throw new IOException();
+        }
+    }
+
+    private static class TestFileReader extends FileReader {
+        public TestFileReader(String fileName) throws FileNotFoundException {
+            super(fileName);
+        }
+
+        @Override
+        public void close() throws IOException {
+            throw new IOException();
         }
     }
 }
